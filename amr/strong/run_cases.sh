5eb305b671130718e2f6cd4db4b45ee32cdd7dc9
Marc Henry de Frahan <marc.henrydefrahan@nrel.gov>
2018-02-05 14:23:49 -0700

    Added some zero padding

--- amr/strong/run_cases.sh
+++ amr/strong/run_cases.sh
@@ -31,11 +31,11 @@ run_cases () {
     do
         echo "Running with ${NRANK} ranks and ${THREADS} threads (${NPROC} procs) and ${NLVL} levels"
         
-        srun -n ${TASKS} -c ${CPUS_PER_TASKS} --cpu_bind=sockets ${PELECBIN} ${INAME} amr.initial_grid_file=${GPFX}${NLVL} amr.n_cell=${NCELLS} ${NCELLS} ${NCELLS} pelec.use_explicit_filter=0 > `printf "0pts_%01dlvls_%05dprocs.out" ${NLVL} ${NPROC}` 2>&1 ;
-        srun -n ${TASKS} -c ${CPUS_PER_TASKS} --cpu_bind=sockets ${PELECBIN} ${INAME} amr.initial_grid_file=${GPFX}${NLVL} amr.n_cell=${NCELLS} ${NCELLS} ${NCELLS} pelec.les_filter_type=0 > `printf "1pts_%01dlvls_%05dprocs.out" ${NLVL} ${NPROCS}` 2>&1 ;
-        srun -n ${TASKS} -c ${CPUS_PER_TASKS} --cpu_bind=sockets ${PELECBIN} ${INAME} amr.initial_grid_file=${GPFX}${NLVL} amr.n_cell=${NCELLS} ${NCELLS} ${NCELLS} pelec.les_filter_fgr=2 > `printf "3pts_%01dlvls_%05dprocs.out" ${NLVL} ${NPROCS}` 2>&1 ;
-        srun -n ${TASKS} -c ${CPUS_PER_TASKS} --cpu_bind=sockets ${PELECBIN} ${INAME} amr.initial_grid_file=${GPFX}${NLVL} amr.n_cell=${NCELLS} ${NCELLS} ${NCELLS} pelec.les_filter_fgr=4 > `printf "5pts_%01dlvls_%05dprocs.out" ${NLVL} ${NPROCS}` 2>&1 ;
-        srun -n ${TASKS} -c ${CPUS_PER_TASKS} --cpu_bind=sockets ${PELECBIN} ${INAME} amr.initial_grid_file=${GPFX}${NLVL} amr.n_cell=${NCELLS} ${NCELLS} ${NCELLS} pelec.les_filter_fgr=6 > `printf "7pts_%01dlvls_%05dprocs.out" ${NLVL} ${NPROCS}` 2>&1 ;
+        srun -n ${TASKS} -c ${CPUS_PER_TASKS} --cpu_bind=sockets ${PELECBIN} ${INAME} amr.initial_grid_file=${GPFX}${NLVL} amr.n_cell=${NCELLS} ${NCELLS} ${NCELLS} pelec.use_explicit_filter=0 > `printf "0pts_%01dlvls_%08dprocs.out" ${NLVL} ${NPROC}` 2>&1 ;
+        srun -n ${TASKS} -c ${CPUS_PER_TASKS} --cpu_bind=sockets ${PELECBIN} ${INAME} amr.initial_grid_file=${GPFX}${NLVL} amr.n_cell=${NCELLS} ${NCELLS} ${NCELLS} pelec.les_filter_type=0 > `printf "1pts_%01dlvls_%08dprocs.out" ${NLVL} ${NPROCS}` 2>&1 ;
+        srun -n ${TASKS} -c ${CPUS_PER_TASKS} --cpu_bind=sockets ${PELECBIN} ${INAME} amr.initial_grid_file=${GPFX}${NLVL} amr.n_cell=${NCELLS} ${NCELLS} ${NCELLS} pelec.les_filter_fgr=2 > `printf "3pts_%01dlvls_%08dprocs.out" ${NLVL} ${NPROCS}` 2>&1 ;
+        srun -n ${TASKS} -c ${CPUS_PER_TASKS} --cpu_bind=sockets ${PELECBIN} ${INAME} amr.initial_grid_file=${GPFX}${NLVL} amr.n_cell=${NCELLS} ${NCELLS} ${NCELLS} pelec.les_filter_fgr=4 > `printf "5pts_%01dlvls_%08dprocs.out" ${NLVL} ${NPROCS}` 2>&1 ;
+        srun -n ${TASKS} -c ${CPUS_PER_TASKS} --cpu_bind=sockets ${PELECBIN} ${INAME} amr.initial_grid_file=${GPFX}${NLVL} amr.n_cell=${NCELLS} ${NCELLS} ${NCELLS} pelec.les_filter_fgr=6 > `printf "7pts_%01dlvls_%08dprocs.out" ${NLVL} ${NPROCS}` 2>&1 ;
     done
 
 
