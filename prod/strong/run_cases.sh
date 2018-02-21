205f89b7a4beccbfb0aba0043a6bb07aa0446db1
Marc Henry de Frahan <marc.henrydefrahan@nrel.gov>
2018-02-21 10:30:51 -0800

    Minor changes

--- prod/strong/run_cases.sh
+++ prod/strong/run_cases.sh
@@ -5,7 +5,7 @@ run_cases () {
     # Setup
     PELECBIN=../../PeleC3d.gnu.ivybridge.PROF.MPI.OMP.ex
     INAME=inputs_3d
-    NLVLS=(1 2 3)
+    NLVLS=(1)
 
     HYPERTHREADS=2
     TASKS_PER_NODE=2
@@ -27,10 +27,10 @@ run_cases () {
     do
 	echo "Running ${NLVL} levels with ${TASKS} tasks and ${THREADS} threads on ${SLURM_JOB_NUM_NODES} nodes (${CORES} cores)"
 
-        (set -x; srun -n ${TASKS} -c ${CPUS_PER_TASKS} --cpu_bind=sockets ${PELECBIN} ${INAME} amr.max_level=${NLVL} amr.n_cell=${NCELLS} ${NCELLS} ${NCELLS} pelec.use_explicit_filter=0 > `printf "0pts_%01dlvls_%08dcores.out" ${NLVL} ${NPROC}` 2>&1 ;)
-        (set -x; srun -n ${TASKS} -c ${CPUS_PER_TASKS} --cpu_bind=sockets ${PELECBIN} ${INAME} amr.max_level=${NLVL} amr.n_cell=${NCELLS} ${NCELLS} ${NCELLS} pelec.les_filter_type=0 > `printf "1pts_%01dlvls_%08dcores.out" ${NLVL} ${CORES}` 2>&1 ;)
+        #(set -x; srun -n ${TASKS} -c ${CPUS_PER_TASKS} --cpu_bind=sockets ${PELECBIN} ${INAME} amr.max_level=${NLVL} amr.n_cell=${NCELLS} ${NCELLS} ${NCELLS} pelec.use_explicit_filter=0 > `printf "0pts_%01dlvls_%08dcores.out" ${NLVL} ${CORES}` 2>&1 ;)
+        #(set -x; srun -n ${TASKS} -c ${CPUS_PER_TASKS} --cpu_bind=sockets ${PELECBIN} ${INAME} amr.max_level=${NLVL} amr.n_cell=${NCELLS} ${NCELLS} ${NCELLS} pelec.les_filter_type=0 > `printf "1pts_%01dlvls_%08dcores.out" ${NLVL} ${CORES}` 2>&1 ;)
         (set -x; srun -n ${TASKS} -c ${CPUS_PER_TASKS} --cpu_bind=sockets ${PELECBIN} ${INAME} amr.max_level=${NLVL} amr.n_cell=${NCELLS} ${NCELLS} ${NCELLS} pelec.les_filter_fgr=2 > `printf "3pts_%01dlvls_%08dcores.out" ${NLVL} ${CORES}` 2>&1 ;)
-        (set -x; srun -n ${TASKS} -c ${CPUS_PER_TASKS} --cpu_bind=sockets ${PELECBIN} ${INAME} amr.max_level=${NLVL} amr.n_cell=${NCELLS} ${NCELLS} ${NCELLS} pelec.les_filter_fgr=4 > `printf "5pts_%01dlvls_%08dcores.out" ${NLVL} ${CORES}` 2>&1 ;)
-        (set -x; srun -n ${TASKS} -c ${CPUS_PER_TASKS} --cpu_bind=sockets ${PELECBIN} ${INAME} amr.max_level=${NLVL} amr.n_cell=${NCELLS} ${NCELLS} ${NCELLS} pelec.les_filter_fgr=6 > `printf "7pts_%01dlvls_%08dcores.out" ${NLVL} ${CORES}` 2>&1 ;)
+        #(set -x; srun -n ${TASKS} -c ${CPUS_PER_TASKS} --cpu_bind=sockets ${PELECBIN} ${INAME} amr.max_level=${NLVL} amr.n_cell=${NCELLS} ${NCELLS} ${NCELLS} pelec.les_filter_fgr=4 > `printf "5pts_%01dlvls_%08dcores.out" ${NLVL} ${CORES}` 2>&1 ;)
+        #(set -x; srun -n ${TASKS} -c ${CPUS_PER_TASKS} --cpu_bind=sockets ${PELECBIN} ${INAME} amr.max_level=${NLVL} amr.n_cell=${NCELLS} ${NCELLS} ${NCELLS} pelec.les_filter_fgr=6 > `printf "7pts_%01dlvls_%08dcores.out" ${NLVL} ${CORES}` 2>&1 ;)
     done
 }
